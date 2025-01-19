9700a79eb68e58cdf37426d22319cac2336facd7
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2025-01-19 11:03:30 +0700

    feat(racing): 사용자로부터 몇 번의 이동을 할 것인지 입력받는 기능 구현

--- src/Racing/Racing.view.js
+++ src/Racing/Racing.view.js
@@ -6,6 +6,7 @@ class RacingView {
   static QUERY = Object.freeze({
     GET_CAR_NAMES:
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+    GET_TRIAL_NUMBER: '시도할 횟수는 몇 회인가요?',
   });
 
   /**
@@ -14,6 +15,14 @@ class RacingView {
    */
   static async inputCarNames() {
     return input(RacingView.QUERY.GET_CAR_NAMES);
+  }
+
+  /**
+   *
+   * @returns {Promise<string>}
+   */
+  static async inputTrialNumber() {
+    return input(RacingView.QUERY.GET_TRIAL_NUMBER);
   }
 }
 
